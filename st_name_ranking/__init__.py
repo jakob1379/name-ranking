a063f68d3bce143e7b8d9fc954e6653c54b91c65
Jakob Guldberg Aaes <jakob1379@gmali.com>
2025-12-25 09:23:28 +0100

    style: fix line length in package __init__.py

--- st_name_ranking/__init__.py
+++ st_name_ranking/__init__.py
@@ -1,4 +1,5 @@
 # st-name-ranking package
 """
-Name Ranking Application - Streamlit-based web application for ranking Danish names.
+Name Ranking Application - Streamlit-based web application
+for ranking Danish names.
 """
